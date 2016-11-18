0a1e03e938f1021e26c90ab36bf81de2f8ecba34
pxeli <hugues.polaert@gmail.com>
2016-11-18 17:16:38 +0100

    Http interfaces

---
+++ tests/P8PTest/Http/StreamTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * P8P Framework - https://github.com/hpolaert/p8p
+*
+* @link      https://github.com/hpolaert/p8p
+* @copyright Copyright (c) 2016 Hugues Polaert
+* @license   https://github.com/hpolaert/p8p/LICENCE.md (MIT)
+*/
+namespace P8PTest\Core;
+
+use P8P\Http\Stream;
+
+/**
+ * HTTP Stream Test Cases
+ *
+ * Asserts P8P\Http\Stream functionalities
+ */
+class StreamTest extends \PHPUnit_Framework_TestCase
+{
+
+}
